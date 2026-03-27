0ee9c7f3be38cf35394acf3068aad4e5088dc2bb
DaphneDana <nakamyukadaphne62@gmail.com>
2026-03-27 16:25:47 +0300

    added comments to the serializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # accepted on input, never returned in response
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'is_job_seeker', 'is_admin']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        # create_user() hashes the password 
+        return User.objects.create_user(**validated_data)
