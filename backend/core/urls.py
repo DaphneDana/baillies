e6d1af8aec29666453ebb87e5fb9b12072e8b4a8
DaphneDana <nakamyukadaphne62@gmail.com>
2026-03-27 16:27:38 +0300

    added comments

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
-    path('api/jobs/', include('jobs.urls')),
+    path('api/auth/', include('accounts.urls')),  # /api/auth/login/, /api/auth/register/, etc.
+    path('api/jobs/', include('jobs.urls')),       # /api/jobs/, /api/jobs/5/, etc.
 ]
-
